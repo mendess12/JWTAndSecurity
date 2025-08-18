5962ca248b54a75d5272a61e00d6f103a4599b6a
yusufmendes <yusufmendes123@gmail.com>
2025-08-18 22:10:30 +0300

    Add project files

---
+++ src/main/java/com/yusufmendes/jwtandsecurity/JwtAndSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.yusufmendes.jwtandsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAndSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtAndSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yusufmendes/jwtandsecurity/JwtAndSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yusufmendes.jwtandsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAndSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
